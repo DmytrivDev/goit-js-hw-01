a210b99413c2d7cbdfb4df47bdfb60d8832f144e
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2023-11-20 18:07:26 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
+    const generalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${generalPrice} credits!`;
 };
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
